6f638547d27a639aa7024e1318cc81d5b63d0d45
Lilly Williams <williams.bryson00@gmail.com>
2025-06-09 20:54:45 -0600

    submit!

--- screens/content/Account.js
+++ screens/content/Account.js
@@ -1,17 +1,46 @@
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Alert } from "react-native";
 import BackgroundTexture from "../../components/ui/BackgroundTexture";
 import Button from "../../components/ui/Button";
 import { Colors } from "../../constants/Colors";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { authContext } from "../../store/auth-context";
 import { userContext } from "../../store/user-context";
+import * as ImagePicker from 'expo-image-picker';
 
 function Account(){
     const authCtx = useContext(authContext);
     const userCtx = useContext(userContext);
+    const [cameraPermissionStatus, requestCameraPermission] = ImagePicker.useCameraPermissions();
+
+    const [image, setImage] = useState();
 
     function logoutHandler() {
         authCtx.logout();
+    }
+
+    const pickImageHandler = async()=>{
+        const result = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [1,1]
+        });
+        if(!result.canceled){
+            setImage(result.assets[0].uri);
+            userCtx.setProfilePic(result.assets[0].uri, authCtx.token);
+        }
+    }
+
+    const takeImageHandler = async()=>{
+        if(cameraPermissionStatus.status == 'undetermined' || !cameraPermissionStatus.granted){
+            requestCameraPermission();
+        }
+        const result = await ImagePicker.launchCameraAsync({
+            allowsEditing: true,
+            aspect: [1,1]
+        });
+        if(!result.canceled){
+            setImage(result.assets[0].uri);
+            userCtx.setProfilePic(result.assets[0].uri, authCtx.token);
+        }
     }
     
     return(
@@ -21,8 +50,8 @@ function Account(){
                 source={!!userCtx.profilePic? {uri:userCtx.profilePic}:require('../../images/defaultPFP.png')}
                 style={styles.profilePic} 
                 />
-                <Button onPress={()=>{}} color={Colors.coolB600}>Change Profile Picture</Button>
-                <Button onPress={()=>{}} color={Colors.warmA400}>Change Password</Button>
+                <Button onPress={pickImageHandler} color={Colors.coolB600}>Pick Profile Picture</Button>
+                <Button onPress={takeImageHandler} color={Colors.warmA400}>Take new Profile Picture</Button>
                 <Button onPress={logoutHandler} color={Colors.warmB700}>Logout</Button>
             </View>
         </BackgroundTexture>
